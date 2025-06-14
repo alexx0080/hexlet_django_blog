645f23d6f02e1b594c1238be57035df2db8bdbf2
Zen Alexey <alexeyzen2008@gmail.com>
2025-06-14 22:38:47 +0700

    af

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -18,6 +18,6 @@ def article_id_and_tag(request, article_id, tags):
     context = {
         'id':article_id,
         'tag':tags,
-        'path':reverse('article', kwargs={'article_id':0, 'tags':'Base article'})
+        'path':reverse('article', kwargs={'article_id': 0, 'tags':'Base article'})
     }
     return render(request, 'article/id_and_tag.html', context=context)
