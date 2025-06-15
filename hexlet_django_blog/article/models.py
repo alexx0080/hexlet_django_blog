b94e0712126a7571d6e4bdd7a09dc11b4fa2b372
Zen Alexey <alexeyzen2008@gmail.com>
2025-06-15 15:57:02 +0700

    Сделали первую модель, и первую миграцию. Модель с models.py (там еще написал небольшую документацию), список миграций в папке migrations приложения

--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+# Для создания таблицы в базе данных нужно в терминале написать python manage.py makemigrations
+# Для последующих изменений использовать migrate
+# Посмотреть все миграции можно в папке migrations приложения
+# Посмотреть sql запрос можно командой sqlmigrate имя_приложение номер_миграции
+# Настройки базы данных в settings.py
+
+# Создаем модель Article
+class Article(models.Model):
+    name = models.CharField(max_length=200)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
