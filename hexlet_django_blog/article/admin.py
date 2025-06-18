049e3e06e07bb254146a5dc45e65530e2fab9007
Zen Alexey <alexeyzen2008@gmail.com>
2025-06-18 13:52:00 +0700

    мы создали админа с помощью команды в терминале manage.py createsuperuser, зарегестрировали модель Article в admin.py чтобы таблица отображалась в админке, а также настроили поиск и отображение элементов с помощью создания класса AdminArticle, который наследуется от класса admin.ModelAdmin

--- hexlet_django_blog/article/admin.py
+++ hexlet_django_blog/article/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
-
+from hexlet_django_blog.article.models import Article
+from django.contrib.admin import DateFieldListFilter
 # Register your models here.
+
+# Сделаем поиск по статьям
+# Прикольная штука, позволяет в админке вбивать строку в поиск и ищет эту подстроку в указанных полях
+# В данном случае name и body
+class AdminArticle(admin.ModelAdmin):
+    # Перечисляем поля, отображаемые в таблице списка статей, метод __str__ можно не использовать если используем это
+    list_display = ('id', 'name', 'created_at')
+    # Перечисляем поля по которым производится поиск
+    search_fields = ['name', 'body']
+    list_filter = (('created_at', DateFieldListFilter),)
+
+
+# Зарегестрируем модели
+admin.site.register(Article, AdminArticle)
+
+
+# Можно сделать подругому и улучшить читаемость кода:
+
+# @admin.register(Article)
+# class AdminArticle(admin.ModelAdmin):
+#     search_fields = ['name', 'body']
+
+# Но мне больше нравится первый вариант, он попонятнее
--- hexlet_django_blog/article/models.py
+++ hexlet_django_blog/article/models.py
@@ -14,3 +14,7 @@ class Article(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     update_at = models.DateTimeField(auto_now=True)
+
+    # Настроим отображение названий объектов в админке
+    # def __str__(self):
+    #     return f'{self.id} {self.name}'
